91f3d82c825b2c589b884d28767ee3be31f6b163
Björn Westerberg Nauclér <mail@bnaucler.se>
2024-01-14 13:14:02 +0100

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,174 @@
+const wdata = [
+
+    {
+        "hdr":"Projects (selection)",
+        "data": [
+            {
+                "title":"Change Course",
+                "when":"2019-2023",
+                "loco":"Väddö (SE)",
+                "text":"Developing and managing the 1-2 years long folk high school program Change Course. This includes everything from syllabus & curriculum education to delivery of classes, staffing, marketing, budget ownership and student admissions.",
+            },
+
+            {
+                "title":"Leadership training and coaching",
+                "when":"2017-2020",
+                "loco":"Multiple clients (NL)",
+                "text":"After developing a methodology and toolkit for leadership training and team processes I delivered tailor-made workshops to a wide array of organizations. The workshops are interactive in nature, giving participants a strong sense of ownership of the final outcome. Except for workshops I have also provided coaching sessions for individuals, created workbooks and graphical models to aid the set purpose.",
+            },
+
+            {
+                "title":"IT Coordinator Benelux",
+                "when":"2015-2016",
+                "loco":"Sandvik Benelux, Amsterdam (NL)",
+                "text":"As IT Coordinator for Benelux I had the main responsibility for delivery of IT services to all internal customers in The Netherlands and Belgium. This included being the direct manager of IT resources i the country, and serve all (approx 800) internal end users with an array of IT services. The assignment included ownership of the IT budget, development of an IT service catalogue, deployment and execution of said services. In this role I worked closely together with managing directors of all individual sites, as well as the heads of other internal service organizations.",
+            },
+
+            {
+                "title":"Moving the SMC HQ",
+                "when":"2014-2015",
+                "loco":"Sandvik Mining, Amsterdam (NL)",
+                "text":"The first assignment for my Amsterdam-based management consultancy agency, B Nauclér Consulting, was to lead the move of the ICT infrastructure as Sandvik Mining moved to new headquarters. The project was delivered under extreme time pressure, and required a good sense of overview and coordination between multiple parties, as well as applying methods for both long-term planning and swift decision making.",
+            },
+
+            {
+                "title":"Burning Land",
+                "when":"2013-2016",
+                "loco":"South Africa",
+                "text":"Burning Land was a in-depth pre-study in the possibilities for building an intentional community in South Africa based on the principles of Burning Man. I worked in close collaboration with Afrikaburn and coordinated a group of volunteers.",
+            },
+
+            {
+                "title":"Kaospilots Cape Town organization",
+                "when":"2013",
+                "loco":"Kaospilot, Cape Town (ZA)",
+                "text":"Given the unique opportunity to design an organization consisting of 36 members I worked with many different aspects of leadership, coordination, communication and process implementation. As a head of the organization my responsibilities included long term resource planning, motivation, mediation and evaluation.",
+            },
+
+            {
+                "title":"Telefónica process consultation",
+                "when":"2012-2013",
+                "loco":"Telefónica Digital, Barcelona (ES)",
+                "text":"As a process manager at Telefónica Digital in Barcelona I worked with the staff to create a better working environment, bridging the gaps between different departments and nurturing innovation throughout the organization. I used coaching tools together with other interview techniques, created real life games and designed processes and interventions for the staff members.",
+            },
+
+            {
+                "title":"Sumatran Orangutan Society",
+                "when":"2012",
+                "loco":"Denmark, London and Indonesia",
+                "text":"As a project leader for a product development project for the NGO Sumatran Orangutan Society my work tasks included coordination between a work group in three different time zones, product ideation and business plan development.",
+            },
+
+            {
+                "title":"1 Mile clock",
+                "when":"2011",
+                "loco":"Burning Man, Nevada (US)",
+                "text":"I was a part of the team who, led by the artist Jim Bowers, built the worlds' largest clock. The record can be seen in Guinness book of world records under the title 'Worlds largest timepiece.'",
+            },
+
+            {
+                "title":"Trafikverket - client migration",
+                "when":"2010",
+                "loco":"Trafikverket ICT, Borlänge (SE)",
+                "text":"I had the role as a project administrator and sub-project manager when the Swedish railroad- and road network- authorities were merged to one organization. My role was to coordinate the migration of about 9000 client computers to a new system without disturbing the essential functionality, while providing the users with information about the new systems.",
+            },
+
+            {
+                "title":"V",
+                "when":"2006",
+                "loco":"Skinnskatteberg (SE)",
+                "text":"I wrote and directed a theater play where the whole manuscript is an alliteration on the letter V. I also built the scenography, cast the actors, set up the lights and sound, and was involved in writing the music which was then performed live on stage.",
+            },
+
+        ]
+    },
+
+    {
+        "hdr":"Employment",
+        "data": [
+            {
+                "title":"Program manager / educator",
+                "when":"2019-current",
+                "loco":"Väddö Folkhögskola, Väddö (SE)",
+                "text":"At Väddö Folkhögskola I have been developing and delivering educational programs in the fields of design, entrepreneurship and social change. The programs have been regular folk high school programs, yrkeshögskola and custom designed programs for corporate clients."
+            },
+
+            {
+                "title":"Lead startup trainer / advisor",
+                "when":"2017-2018",
+                "loco":"DELITELABS, Amsterdam (NL)",
+                "text":"As a startup advisor at DELITELABS I engaged in activities like program development, delivering trainings in Lean Startup and Leadership as well as providing individual and team coaching to course participants."
+            },
+
+            {
+                "title":"Project- & process leader",
+                "when":"2014-2020",
+                "loco":"B Nauclér Consulting, Amsterdam (NL)",
+                "text":"Shortly after moving to Amsterdam I started my own management consultancy. B Nauclér Consulting delivered services within the fields of organizational development, project leadership, process leadership and coaching to a multitude of clients."
+
+            },
+
+            {
+                "title":"Project manager & administrator",
+                "when":"2009-2014",
+                "loco":"Logica Infra / CGI, Gävle (SE)",
+                "text":"My position at Logica Infra (later merged with CGI) developed to circle more and more around people and less around technological solutions. I was involved in multiple projects as a project manager or administrator. Customers include major phone companies and the Swedish government. Projects have included service implementation, company mergers and systemic organization-wide changes."
+            },
+
+            {
+                "title":"Technical systems administrator",
+                "when":"2007-2009",
+                "loco":"Logica Infra / Sandvik, Sandviken (SE)",
+                "text":"As a consultant from Logica my first assignment was with Sandvik IT where I did design and manage systems for large scale data processing. I learned a lot about corporate culture, teamwork, organization and administration while further developing my technical skills."
+            },
+
+            {
+                "title":"Bartender",
+                "when":"2003-2004",
+                "loco":"The Chieftain & The Gregorian (UK)",
+                "text":"I spent about a year traveling the British isles and tending bars. A few cities I lived in during this time period are London, Oxford and Edinburgh. Here I trained my English, my customer service skills and did for the first time of my life get totally emerged in a non-Scandinavian culture."
+            },
+
+            {
+                "title":"Technichian / consultant / educator / administrator",
+                "when":"2000-2003",
+                "loco":"Solvus Support AB, Järvsö (SE)",
+                "text":"My original position on Solvus Support AB in Järvsö, Sweden was as a support technician; receiving phone calls and resolving home computer issues. During my time within the company my role was expanded to mainly focuse on large scale networking equipment - doing consultant work and education. I did also administer the UNIX servers of the company. On Solvus i learned a lot about human interaction as I was primarily doing service work. I did also gain a lot of insight in the fields of technology and pedagogy."
+            },
+        ]
+    },
+
+    {
+        "hdr":"Education",
+        "data": [
+            {
+                "title":"Kaospilot: business, design & leadership",
+                "when":"2011-2014",
+                "loco":"Kaospilot, Aarhus (DK)",
+                "text":"Kaospilot is a school educating enterprising leaders; mainly focused on the fields of: creative project design, creative business design and creative process design. The education is hands-on by nature, and as a student at the school you meanwhile undertake projects for real-life clients. My main areas of focus have been in process design, educational design and the 'softer sides' of leadership."
+            },
+
+            {
+                "title":"Theater; acting and directing",
+                "when":"2005-2006",
+                "loco":"Skinnskattebergs Folkhögskola, Skinnskatteberg (SE)",
+                "text":"I undertook a multidisciplinary theater education where I practiced mainly acting, but also directing, playwriting, propmaking, sound & light setup and control."
+            },
+
+            {
+                "title":"Journalism and peace/conflict studies",
+                "when":"2005-2006",
+                "loco":"Skinnskattebergs Folkhögskola, Skinnskatteberg (SE)",
+                "text":"As a part of my studies in journalism I travelled to Northern Ireland and wrote for a magazine about the troubles. My special focus was the early history of Northern Ireland and how this laid the foundation for the troubles during the second half of the 1900s.",
+            },
+
+            {
+                "title":"Gymnasium - Natural sciences",
+                "when":"1998-2000",
+                "loco":"Slottegymnasiet, Ljusdal (SE)",
+                "text":"Gymnasial education in the field of natural sciences, specialized in information technology and C++ programming."
+            },
+
+        ]
+    },
+]
---
+++ js/ldata.js
@@ -0,0 +1,99 @@
+const gid = document.getElementById.bind(document);
+const gls = localStorage.getItem.bind(localStorage);
+
+let theme;
+
+// Creates DOM object, setting type, class and inner text
+function mkobj(type, cl, txt) {
+
+    let ret = document.createElement(type);
+
+    if(cl !== undefined && cl != "") ret.classList.add(cl);
+
+    if(txt !== undefined) {
+        const tc = document.createTextNode(txt);
+        ret.appendChild(tc);
+    }
+
+    return ret;
+}
+
+// Add section to data
+function addsection(sec) {
+
+    const hline = mkobj("div", "hline");
+    const hdr = mkobj("h2", "hcol", sec.hdr);
+    const gap = mkobj("div", "smallgap");
+    const pdiv = gid("data");
+
+    pdiv.appendChild(hline);
+    pdiv.appendChild(hdr);
+    pdiv.appendChild(gap);
+
+    for(const o of sec.data) {
+        let sect = mkobj("h3", "hcol", o.title);
+        let secw = mkobj("div", "secdate", o.when);
+        let secl = mkobj("div", "secloc", o.loco);
+        let secd = mkobj("div", "secdata", o.text);
+        let sgap = mkobj("div", "smallgap");
+
+        pdiv.appendChild(sect);
+        pdiv.appendChild(secw);
+        pdiv.appendChild(secl);
+        pdiv.appendChild(secd);
+        pdiv.appendChild(sgap);
+    }
+}
+
+// Toggles between light and dark theme
+function toggletheme() {
+    if(theme == "dark.css") theme = "light.css";
+    else theme = "dark.css";
+
+    settheme(theme);
+}
+
+// Activates selected theme & load stylesheet
+function settheme(theme) {
+
+    gid("theme").href = "css/" + theme;
+    gid("ssel").innerHTML = theme == "dark.css" ? "L" : "D";
+
+    localStorage.setItem("bnaucler-se-theme", theme);
+}
+
+// Scrolls smoothly to top of page
+function scrollup() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+// Captures scrolling event and conditionally displays top button
+function scrollcap() {
+
+    const y = window.scrollY;
+    const tbtn = gid("scrollup");
+
+    if(y > 500) tbtn.style.display = "block";
+    else tbtn.style.display = "none";
+}
+
+// Initialize page
+function init() {
+
+    theme = gls("bnaucler-se-theme");
+    if(theme == undefined || theme == "") theme = "dark.css";
+    settheme(theme);
+
+    const ddiv = gid("data");
+
+    ddiv.innerHTML = "";
+
+    for(const o of wdata) {
+        addsection(o);
+    }
+}
+
+// Call init on (re)load
+window.onload = () => init();
+window.onbeforeunload = () => init();
+window.addEventListener("scroll", scrollcap);
